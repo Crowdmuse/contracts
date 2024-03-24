a38cab385c0b5181d8c86985604dc68717348eae
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-24 18:07:20 -0300

    I add NATSPEC to ICrowdmuseProduct.

--- src/interfaces/ICrowdmuseProduct.sol
+++ src/interfaces/ICrowdmuseProduct.sol
@@ -3,12 +3,18 @@
 pragma solidity ^0.8.10;
 
 interface ICrowdmuseProduct {
+    /// @notice Enumeration for task statuses
     enum TaskStatus {
         Open,
         Assigned,
         Complete
     }
 
+    /// @notice Structure for defining tasks within a project
+    /// @param contributionValues The value of contributions for the task
+    /// @param taskContributors Addresses of contributors to the task
+    /// @param taskStatus Current status of each task
+    /// @param taskContributorTypes Types of contributors for the task
     struct Task {
         uint256[] contributionValues;
         address[] taskContributors;
@@ -16,6 +22,11 @@ interface ICrowdmuseProduct {
         uint256[] taskContributorTypes;
     }
 
+    /// @notice Structure for defining token metadata and minting configurations
+    /// @param productName Name of the product or NFT collection
+    /// @param productSymbol Symbol of the NFT collection
+    /// @param baseUri Base URI for NFT metadata
+    /// @param maxAmountOfTokensPerMint Maximum number of tokens that can be minted in one transaction
     struct Token {
         string productName;
         string productSymbol;
@@ -23,16 +34,21 @@ interface ICrowdmuseProduct {
         uint256 maxAmountOfTokensPerMint;
     }
 
+    /// @notice Structure for managing inventory of a product
+    /// @param keyName Unique key for inventory item
+    /// @param garmentsRemaining Number of items remaining in this inventory category
     struct Inventory {
         string keyName;
         uint96 garmentsRemaining;
     }
 
+    /// @notice Enumeration for the status of the product
     enum ProductStatus {
         InProgress,
         Complete
     }
 
+    /// @notice Enumeration for types of NFTs that can be minted
     enum NFTTypes {
         Default,
         Product,
@@ -40,6 +56,17 @@ interface ICrowdmuseProduct {
         Investor
     }
 
+    /// @notice Detailed information about a task within the project
+    /// @param taskId Unique identifier for the task
+    /// @param contributionValue Value of the contribution towards the task
+    /// @param taskOwnerAddress Address of the task owner
+    /// @param taskContributor Address of the task contributor
+    /// @param licensedProjects Array of project identifiers this task is licensed for
+    /// @param feedbackScore Score given as feedback for the task completion
+    /// @param submissionUri URI for the task submission details
+    /// @param taskMetadataUri URI for metadata related to the task
+    /// @param taskStatus Current status of the task
+    /// @param taskType Numerical type of the task
     struct TaskInformation {
         uint256 taskId;
         uint256 contributionValue;
