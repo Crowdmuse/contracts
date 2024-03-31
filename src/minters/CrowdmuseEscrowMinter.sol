2048ef265834f0d1443ca0f86928b301e7ff61cf
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-31 07:16:38 -0300

    I initialize CrowdmuseEscrowMinter.

---
+++ src/minters/CrowdmuseEscrowMinter.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IMinterErrors} from "../interfaces/IMinterErrors.sol";
+import {ICrowdmuseProduct} from "../interfaces/ICrowdmuseProduct.sol";
+
+/// @title CrowdmuseEscrowMinter
+/// @notice A minter that allows for basic purchasing on Crowdmuse
+contract CrowdmuseEscrowMinter is IMinterErrors {
+    /// @notice Retrieves the contract metadata URI
+    /// @return A string representing the metadata URI for this contract
+    function contractURI() external pure returns (string memory) {
+        return "https://github.com/Crowdmuse/contracts";
+    }
+
+    /// @notice Retrieves the name of the minter contract
+    /// @return A string representing the name of this minter contract
+    function contractName() external pure returns (string memory) {
+        return "Crowdmuse Basic Minter";
+    }
+
+    /// @notice Retrieves the version of the minter contract
+    /// @return A string representing the version of this minter contract
+    function contractVersion() external pure returns (string memory) {
+        return "0.0.1";
+    }
+
+    /// @dev Emitted when a mint operation includes a comment
+    /// @param sender The address that initiated the mint operation
+    /// @param tokenContract The address of the token contract where the mint occurred
+    /// @param tokenId The ID of the token that was minted
+    /// @param quantity The quantity of tokens minted
+    /// @param comment A comment provided during the minting process
+    event MintComment(
+        address indexed sender,
+        address indexed tokenContract,
+        uint256 indexed tokenId,
+        uint256 quantity,
+        string comment
+    );
+
+    /// @notice Mints tokens to a specified address with an optional comment
+    /// @param target The target CrowdmuseProduct contract address where the mint will occur
+    /// @param mintTo The address that will receive the minted tokens
+    /// @param garmentType The type of garment being minted, represented as a bytes32 hash
+    /// @param quantity The quantity of tokens to mint
+    /// @param comment An optional comment provided for the minting operation
+    /// @return tokenId The token ID of the last minted token
+    function mint(
+        address target,
+        address mintTo,
+        bytes32 garmentType,
+        uint256 quantity,
+        string memory comment
+    ) external returns (uint256 tokenId) {
+        tokenId = _mint(target, mintTo, garmentType, quantity, comment);
+    }
+
+    /// @dev Internal function to handle the minting operation
+    /// @param target The target CrowdmuseProduct contract address where the mint will occur
+    /// @param mintTo The address that will receive the minted tokens
+    /// @param garmentType The type of garment being minted, represented as a bytes32 hash
+    /// @param quantity The quantity of tokens to mint
+    /// @param comment An optional comment provided for the minting operation
+    /// @return tokenId The token ID of the last minted token
+    function _mint(
+        address target,
+        address mintTo,
+        bytes32 garmentType,
+        uint256 quantity,
+        string memory comment
+    ) internal returns (uint256 tokenId) {
+        tokenId = ICrowdmuseProduct(target).buyPrepaidNFT(
+            mintTo,
+            garmentType,
+            quantity
+        );
+
+        if (bytes(comment).length > 0) {
+            emit MintComment(mintTo, target, tokenId, quantity, comment);
+        }
+    }
+}
---
+++ test/minter/CrowdmuseEscrowMinter.t.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import {CrowdmuseProduct} from "../../src/CrowdmuseProduct.sol";
+import {ICrowdmuseProduct} from "../../src/interfaces/ICrowdmuseProduct.sol";
+import {CrowdmuseEscrowMinter} from "../../src/minters/CrowdmuseEscrowMinter.sol";
+import {MockERC20} from "../mocks/MockERC20.sol";
+
+contract CrowdmuseEscrowMinterTest is Test {
+    CrowdmuseProduct internal product;
+    CrowdmuseEscrowMinter internal minter;
+    MockERC20 internal usdc;
+    address payable internal admin = payable(address(0x999));
+    address payable internal protocolFeeRecipient = payable(address(0x7777777));
+    address internal zora;
+    address internal tokenRecipient;
+    address internal fundsRecipient;
+
+    event MintComment(
+        address indexed sender,
+        address indexed tokenContract,
+        uint256 indexed tokenId,
+        uint256 quantity,
+        string comment
+    );
+
+    function setUp() external {
+        zora = makeAddr("zora");
+        tokenRecipient = makeAddr("tokenRecipient");
+        fundsRecipient = makeAddr("fundsRecipient");
+
+        minter = new CrowdmuseEscrowMinter();
+        vm.prank(admin);
+        admin = payable(address(this));
+        usdc = new MockERC20("MockUSD", "MUSD");
+        ICrowdmuseProduct.Token memory tokenInfo = ICrowdmuseProduct.Token({
+            productName: "MyProduct",
+            productSymbol: "MPROD",
+            baseUri: "ipfs://baseuri/",
+            maxAmountOfTokensPerMint: 10
+        });
+        uint256[] memory contributionValues = new uint256[](1);
+        contributionValues[0] = 1000;
+        address[] memory taskContributors = new address[](1);
+        taskContributors[0] = admin;
+        ICrowdmuseProduct.TaskStatus[]
+            memory taskStatuses = new ICrowdmuseProduct.TaskStatus[](1);
+        taskStatuses[0] = ICrowdmuseProduct.TaskStatus.Complete;
+        uint256[] memory taskContributorTypes = new uint256[](1);
+        taskContributorTypes[0] = 1;
+        ICrowdmuseProduct.Task memory initialTask = ICrowdmuseProduct.Task({
+            contributionValues: contributionValues,
+            taskContributors: taskContributors,
+            taskStatus: taskStatuses,
+            taskContributorTypes: taskContributorTypes
+        });
+        ICrowdmuseProduct.Inventory[]
+            memory initialInventory = new ICrowdmuseProduct.Inventory[](1);
+        initialInventory[0] = ICrowdmuseProduct.Inventory({
+            keyName: "size:one",
+            garmentsRemaining: 100
+        });
+
+        product = new CrowdmuseProduct(
+            500, // _feeNumerator
+            10000, // _contributorTotalSupply
+            100, // _garmentsAvailable
+            initialTask,
+            tokenInfo,
+            address(usdc),
+            "InventoryKey",
+            initialInventory,
+            false, // _madeToOrder
+            admin,
+            1 ether // _buyNFTPrice
+        );
+    }
+
+    function test_ContractName() external view {
+        assertEq(minter.contractName(), "Crowdmuse Basic Minter");
+    }
+
+    function test_Version() external view {
+        assertEq(minter.contractVersion(), "0.0.1");
+    }
+
+    function test_MintFlow() external {
+        address recipient = address(0x123);
+        bytes32 garmentType = keccak256(abi.encodePacked("size:one"));
+        uint256 quantity = 1;
+        uint256 initialGarmentsRemaining = product.inventoryGarmentsRemaining(
+            garmentType
+        );
+        uint256 initialRecipientBalance = product.balanceOf(recipient);
+
+        address target = address(product);
+        address mintTo = recipient;
+        string memory comment = "test comment";
+        vm.prank(admin);
+        product.changeAdmin(address(minter));
+
+        vm.startPrank(tokenRecipient);
+        uint256 newTokenId = product.totalSupply() + 1;
+        vm.expectEmit(true, true, true, true);
+        emit MintComment(mintTo, target, newTokenId, quantity, comment);
+        uint256 tokenId = minter.mint(
+            target,
+            mintTo,
+            garmentType,
+            quantity,
+            comment
+        );
+        vm.stopPrank();
+
+        uint256 newGarmentsRemaining = product.inventoryGarmentsRemaining(
+            garmentType
+        );
+        uint256 newRecipientBalance = product.balanceOf(recipient);
+        assertEq(
+            newGarmentsRemaining,
+            initialGarmentsRemaining - quantity,
+            "Garment inventory should decrease by the quantity minted."
+        );
+        assertEq(
+            newRecipientBalance,
+            initialRecipientBalance + quantity,
+            "Recipient should have more NFTs after minting."
+        );
+        bytes32 mintedNFTGarmentType = product.NFTBySize(tokenId);
+        assertEq(
+            mintedNFTGarmentType,
+            garmentType,
+            "The minted NFT should have the correct garment type."
+        );
+    }
+}
